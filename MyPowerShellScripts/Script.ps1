d7ca8a38c1ec499d6bd1b590eb3d7387f65f2f95
Matti Andreas Nielsen <matti@it-kartellet.dk>
2015-05-22 16:32:58 +0200

    my first powershell script that creates alot of files for testing zip functionality.

---
+++ MyPowerShellScripts/Script.ps1
@@ -0,0 +1,19 @@
+﻿#
+# Script.ps1
+#
+$hour = 01
+For($year = 14; $year -le 16; $year++) {
+
+	For($month =01; $month  -le 12; $month++){
+
+		For ($day = 01; $day –le 28; $day++) {
+			$hour++;
+			if($hour -gt 23){
+				$hour = 1
+			}
+			$date = (Get-Date -Month $month -Day $day -Year $year -Hour $hour).tostring("yyMMddHH");
+			out-file -FilePath C:\logdata\logs\globeshare.novonordisk.com\webdkba1085\ex$date.log
+		} 
+	}
+}
+
