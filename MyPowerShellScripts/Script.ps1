4c47c613c07d74f229218d00ce650f50e012fc59
Matti Andreas Nielsen <matti@it-kartellet.dk>
2015-06-10 15:38:18 +0200

    added size of file and a function.

--- MyPowerShellScripts/Script.ps1
+++ MyPowerShellScripts/Script.ps1
@@ -1,18 +1,30 @@
 ﻿#
 # Script.ps1
 #
+function New-EmptyFile
+{
+   param( [string]$FilePath,[double]$Size )
+ 
+   $file = [System.IO.File]::Create($FilePath)
+   $file.SetLength($Size)
+   $file.Close()
+   Get-Item $file.Name
+}
+
+
+
 $hour = 01
-For($year = 14; $year -le 16; $year++) {
+For($year = 14; $year -lt 17; $year++) {
 
-	For($month =01; $month  -le 12; $month++){
+	For($month =01; $month  -lt 13; $month++){
 
-		For ($day = 01; $day –le 28; $day++) {
+		For ($day = 01; $day –lt 28; $day++) {
 			$hour++;
 			if($hour -gt 23){
 				$hour = 1
 			}
 			$date = (Get-Date -Month $month -Day $day -Year $year -Hour $hour).tostring("yyMMddHH");
-			out-file -FilePath C:\logdata\logs\globeshare.novonordisk.com\webdkba1085\ex$date.log
+			New-EmptyFile -FilePath C:\Users\herpderp\Documents\wintra\filepaths\ex$date.log -Size 10mb
 		} 
 	}
 }
